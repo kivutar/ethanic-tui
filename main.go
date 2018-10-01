b9f29d15bffd38d7865773695da5937f78f7ec7d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-10-01 22:33:31 +0200

    Use gocui to display two panes and a modal

--- main.go
+++ main.go
@@ -1,19 +1,146 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
 )
 
-func main() {
-	var version bool
-	flag.BoolVar(&version, "version", false, "version")
-	flag.Parse()
+func switchColumn(g *gocui.Gui, v *gocui.View) error {
+	if v == nil || v.Name() == "side" {
+		_, err := g.SetCurrentView("main")
+		return err
+	}
+	_, err := g.SetCurrentView("side")
+	return err
+}
 
-	if version {
-		fmt.Printf("etui v0.0.1")
-	} else {
-		fmt.Printf("work-in-progress")
-		app()
+func cursorDown(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy+1); err != nil {
+			ox, oy := v.Origin()
+			if err := v.SetOrigin(ox, oy+1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func cursorUp(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+			if err := v.SetOrigin(ox, oy-1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func delModal(g *gocui.Gui, v *gocui.View) error {
+	if err := g.DeleteView("modal"); err != nil {
+		return err
+	}
+	if _, err := g.SetCurrentView("side"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func transferEthModal(g *gocui.Gui, v *gocui.View) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("modal", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Transfer ETH"
+		fmt.Fprintln(v, "Recipient:")
+		if _, err := g.SetCurrentView("modal"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("side", gocui.KeyTab, gocui.ModNone, switchColumn); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("main", gocui.KeyTab, gocui.ModNone, switchColumn); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, transferEthModal); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyCtrlT, gocui.ModNone, transferEthModal); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("modal", gocui.KeyEnter, gocui.ModNone, delModal); err != nil {
+		return err
+	}
+	return nil
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelFgColor = gocui.ColorBlack
+		v.SelBgColor = gocui.ColorYellow
+		fmt.Fprintln(v, "(T)ransfer ETH")
+		fmt.Fprintln(v, "(D)eploy a contract")
+		fmt.Fprintln(v, "(I)nteract with a contract")
+	}
+	if v, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Editable = true
+		v.Wrap = true
+		if _, err := g.SetCurrentView("side"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.Cursor = true
+
+	g.SetManagerFunc(layout)
+
+	if err := keybindings(g); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
 	}
 }
